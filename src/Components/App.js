3221e41acf3f9b0ab72c5dae1b1009d11aa36d21
yanghyunjun <akakak231@gmail.com>
2019-11-19 22:51:35 +0900

    Connect to ApolloClient and localState

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo-hooks";
 import GlobalStyles from "../Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
+import Client from "../Apollo/Client";
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <>
+    <ApolloProvider client={Client}>
       <GlobalStyles />
       <Router isLoggedIn={false} />
-    </>
+    </ApolloProvider>
   </ThemeProvider>
 );
