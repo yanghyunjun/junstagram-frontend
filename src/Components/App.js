4b4b758fe6d87c34c75e3296488a20a975696e57
yanghyunjun <akakak231@gmail.com>
2019-11-20 23:57:44 +0900

    create login Authentication Hooks Query use to token

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { ApolloProvider } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import GlobalStyles from "../Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
-import Client from "../Apollo/Client";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <ApolloProvider client={Client}>
-      <GlobalStyles />
-      <Router isLoggedIn={false} />
-    </ApolloProvider>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
