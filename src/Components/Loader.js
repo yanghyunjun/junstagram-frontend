dcc2b1951bac587b01a4be692cb4648f1f198dd7
yanghyunjun <akakak231@gmail.com>
2019-12-29 23:55:17 +0900

    Create Loader in Feed

---
+++ src/Components/Loader.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled, { Keyframes, keyframes } from "styled-components";
+import { InstaLogo } from "./Icons";
+
+const Animation = keyframes`
+    0%{
+        opacity:0
+    }
+    50%{
+        opacity:1
+    }
+    100%{
+        opacity:0
+    }
+`;
+
+const Loader = styled.div`
+  animation: ${Animation} 1s linear infinite;
+`;
+
+export default () => (
+  <Loader>
+    <InstaLogo size={36} />
+  </Loader>
+);
