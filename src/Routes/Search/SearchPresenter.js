1781b51fa768e35b6323d086263ac4c92a324440
yanghyunjun <akakak231@gmail.com>
2020-01-15 16:32:27 +0900

    Add Search Post view

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
 import Loader from "../../Components/Loader";
 import UserCard from "../../Components/UserCard";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
   height: 50vh;
@@ -13,9 +14,15 @@ const Section = styled.div`
   margin-bottom: 50px;
   display: grid;
   grid-gap: 25px;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 160px);
   grid-template-rows: 160px;
   grid-auto-rows: 160px;
+`;
+
+const PostSection = styled(Section)`
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
 `;
 
 const SearchPresenter = ({ searchTerm, loading, data }) => {
@@ -41,22 +48,29 @@ const SearchPresenter = ({ searchTerm, loading, data }) => {
             data.searchUser.map(user => (
               <UserCard
                 key={user.id}
-                username={user.username}
-                isFollowing={user.isFollowing}
-                url={user.avatar}
-                isSelf={user.isSelf}
                 id={user.id}
+                url={user.avatar}
+                userName={user.userName}
+                isFollowing={user.isFollowing}
+                isSelf={user.isSelf}
               />
             ))
           )}
         </Section>
-        <Section>
+        <PostSection>
           {data.searchPost.length === 0 ? (
             <FatText text="No Posts Found" />
           ) : (
-            data.searchPost.map(post => null)
+            data.searchPost.map(post => (
+              <SquarePost
+                key={post}
+                file={post.files[0]}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+              />
+            ))
           )}
-        </Section>
+        </PostSection>
       </Wrapper>
     );
   }
