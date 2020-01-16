d7a3fadf0fb84e02acba1a50d031bcc35f7d54f4
yanghyunjun <akakak231@gmail.com>
2020-01-17 04:01:50 +0900

    Add LogOut

--- src/Routes/Profile/ProfileContainer.js
+++ src/Routes/Profile/ProfileContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { useQuery } from "react-apollo-hooks";
+import { useQuery, useMutation } from "react-apollo-hooks";
 import { withRouter } from "react-router-dom";
 import ProfilePresenter from "./ProfilePresenter";
 
@@ -18,6 +18,7 @@ const GET_USER = gql`
       followersCount
       postsCount
       posts {
+        id
         files {
           url
         }
@@ -28,6 +29,12 @@ const GET_USER = gql`
   }
 `;
 
+export const LOG_OUT = gql`
+  mutation logUserOut {
+    logUserOut @client
+  }
+`;
+
 export default withRouter(
   ({
     match: {
@@ -35,6 +42,7 @@ export default withRouter(
     }
   }) => {
     const { data, loading } = useQuery(GET_USER, { variables: { username } });
-    return <ProfilePresenter loading={loading} data={data} />;
+    const [logOut] = useMutation(LOG_OUT);
+    return <ProfilePresenter loading={loading} logOut={logOut} data={data} />;
   }
 );
--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -6,6 +6,7 @@ import Avatar from "../../Components/Avatar";
 import FatText from "../../Components/FatText";
 import FollowButton from "../../Components/FollowButton";
 import SquarePost from "../../Components/SquarePost";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -65,7 +66,7 @@ const Posts = styled.div`
   grid-auto-rows: 200px;
 `;
 
-export default ({ loading, data }) => {
+export default ({ loading, data, logOut }) => {
   if (loading === true) {
     return (
       <Wrapper>
@@ -101,7 +102,11 @@ export default ({ loading, data }) => {
             <UsernameRow>
               <Username>{username}</Username>
               <FollowContainer>
-                {!isSelf && <FollowButton id={id} isFollowing={isFollowing} />}
+                {isSelf ? (
+                  <Button onClick={logOut} text="Log Out" />
+                ) : (
+                  <FollowButton id={id} isFollowing={isFollowing} />
+                )}
               </FollowContainer>
             </UsernameRow>
             <Counts>
